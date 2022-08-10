5fbb47184f4fb37e726e3afc1fb1d9f3606e0575
novikov-al-st <novikov.al.st@gmail.com>
2022-08-11 00:56:51 +0300

    Update guessing-game.js
    feat: implement class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
+
+
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return  this.min + Math.round((this.max - this.min) / 2);
     }
 
     lower() {
-
+        this.max = this.min + Math.round((this.max - this.min) / 2);
     }
 
     greater() {
-
+        this.min = this.min + Math.round((this.max - this.min) / 2);
     }
 }
 
